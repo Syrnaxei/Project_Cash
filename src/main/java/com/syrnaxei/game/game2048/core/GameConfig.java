3b9e5ce948da7958348d9781d12274af926e2ee4
Syrnaxei <1725576689@qq.com>
2025-12-18 11:37:50 +0800

    修复分数无法同步bug，倒计时秒数调整

--- src/main/java/com/syrnaxei/game/game2048/core/GameConfig.java
+++ src/main/java/com/syrnaxei/game/game2048/core/GameConfig.java
@@ -4,5 +4,5 @@ public class GameConfig {
     //====================================  静态 变量  ====================================
     public static final int BOARD_SIZE = 4; //棋盘大小
     public static final int S_FOUR_P = 10; //生成4的概率，设为20%
-    public static int remainingSeconds = 5;
+    public static int remainingSeconds = 30;
 }
